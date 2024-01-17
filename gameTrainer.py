91acf1ba99eff53b4e8ba093788ee36d01741300
Benny-glitch <58368441+Benny-glitch@users.noreply.github.com>
2024-01-17 11:52:49 +0100

    the train of the AI is done. Added the agent class that is used for play the game

---
+++ gameTrainer.py
@@ -0,0 +1,36 @@
+import os
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+import vizDoomGymEnv
+
+
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
+
+
+CHECKPOINT_DIR = './train/train_basic'
+LOG_DIR = './logs/log_basic'
+callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
+
+env = vizDoomGymEnv.VizDoomGym(render=True)
+model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, learning_rate=0.0001, n_steps=2048)
+
+model.learn(total_timesteps=100000, callback=callback)
+
--- vizDoomGymEnv.py
+++ vizDoomGymEnv.py
@@ -1,9 +1,8 @@
 from vizdoom import *
-import random
-import time
 import numpy as np
 from gymnasium import Env
 from gymnasium.spaces import Discrete, Box
+from stable_baselines3.common import env_checker
 import cv2
 
 
@@ -12,50 +11,57 @@ class VizDoomGym(Env):
     def __init__(self, render=False):
         # Inherit from Env
         super().__init__()
-        #Setup the game
+        # Set up the game
         self.game = DoomGame()
         self.game.load_config('doomGame/VizDoom/scenarios/basic.cfg')
-        self.game.init()
 
         # Bool Method game render or not
-        if not render:
+        if render == False:
             self.game.set_window_visible(False)
         else:
             self.game.set_window_visible(True)
 
-        # Creation of the action and the obsservation space
+        self.game.init()
+
+        # Creation of the action and the observation space
         self.observation_space = Box(low=0, high=255, shape=(100, 160, 1), dtype=np.uint8)
         self.action_space = Discrete(3)
 
     def step(self, action):
-        actions = np.identity(3, dtype=np.uint8)
-        reward = self.game.make_action(actions[action])
+        actions = np.identity(3)
+        reward = self.game.make_action(actions[action], 4)
 
         if self.game.get_state():
             state = self.game.get_state().screen_buffer
-            state = self.grayScale(state)
-            info = self.game.get_state().game_variables
+            state = self.grayscale(state)
+            ammo = self.game.get_state().game_variables[0]
+            info = {"info": ammo}
         else:
             state = np.zeros(self.observation_space.shape)
-            info = 0
+            info = {"info": 0}
 
         done = self.game.is_episode_finished()
 
-        return state, reward, info, done
+        return state, reward, done, False, info
 
     def close(self):
         self.game.close()
 
     # Method called from reset the Env
-    def resetEnv(self):
+    def reset(self, **kwargs):
         self.game.new_episode()
-        state = self.game.new_episode()
-        return self.grayScale(state)
+        state = self.game.get_state().screen_buffer
+        observations = self.grayscale(state)
+        info = {}  # Usa un dizionario vuoto se non hai informazioni di reset da restituire
+        return observations, info
 
     # GrayScale the game frame and resize
-    def grayScale(self, observation):
-        gray = cv2.cvtColor(np.moveaxis(observation, 0, 1), cv2.COLOR_BGR2GRAY)
+    def grayscale(self, observation):
+        gray = cv2.cvtColor(np.moveaxis(observation, 0, -1), cv2.COLOR_BGR2GRAY)
         resize = cv2.resize(gray, (160, 100), interpolation=cv2.INTER_CUBIC)
-        state = np.reshape(resize, -1, (100, 160, 1))
-
+        state = np.reshape(resize, (100, 160, 1))
         return state
+
+
+env = VizDoomGym()
+env_checker.check_env(env)
