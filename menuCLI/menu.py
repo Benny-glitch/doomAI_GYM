70d4c2efbec76c5f0ae2d23d3307e3d6b8e2284d
Benny-glitch <58368441+Benny-glitch@users.noreply.github.com>
2024-01-18 15:46:30 +0100

    This commit signifies the completion of the graphical interface for the menu.

--- menuCLI/menu.py
+++ menuCLI/menu.py
@@ -1,9 +1,8 @@
-from colorama import Fore, Style, init
 from termcolor import colored
 from pyfiglet import Figlet
 import six
 
-def log(entry_num, string, color, figlet_font):
+def log(string, color, figlet_font):
     if figlet_font is None:
         six.print_(colored(string, color))
     else:
@@ -12,12 +11,24 @@ def log(entry_num, string, color, figlet_font):
 
 
 def options_menu():
-    log("Doom AI Game","blue","slant")
-    log("Benvenuto a Doom AI Game", "light_green", None)
+    log("Doom AI Game","red","slant")
+    log("Benvenuto a Doom AI Game", "dark_grey", None)
+    log("1. Lascia giocare l'AI", "white", None)
+    log("2. Traina l'IA", "white", None)
+    log("3. Lascia giocare l'IA da remoto","white", None)
+    log("4. Carica un modello di training diverso","white", None)
 
+def play_AI():
+    pass
 
+def train_AI():
+    pass
 
+def play_AI_remte():
+    pass
 
+def load_different_model():
+    pass
 
 
 if __name__ == "__main__":
--- sampleEnv.py
+++ sampleEnv.py
@@ -4,6 +4,9 @@ import time
 import numpy as np
 from gymnasium import Env
 from gymnasium.spaces import Discrete, Box
+import pygame
+
+pygame.init()
 
 game = DoomGame()
 game.load_config('doomGame/VizDoom/scenarios/basic.cfg')
@@ -11,15 +14,27 @@ game.init()
 
 actions = np.identity(3, dtype=np.uint8)
 
+
+def takeinput():
+    if pygame.key.get_pressed() == pygame.K_LEFT:
+        return actions[0]
+    if pygame.key.get_pressed() == pygame.K_RIGHT:
+        return actions[1]
+    if pygame.key.get_pressed() == pygame.K_SPACE:
+        return actions[2]
+    return [0, 0, 0]
+
+
 episodes = 10
 for episode in range(episodes):
     game.new_episode()
     while not game.is_episode_finished():
         state = game.get_state()
         info = state.game_variables
-        reward = game.make_action(random.choice(actions))
-        print('reward:', reward)
-        print('info', info)
+        reward = game.make_action(takeinput())
         time.sleep(0.02)
     print('result', game.get_total_reward())
     time.sleep(2)
+print(actions[0])
+print(actions[1])
+print(actions[2])
